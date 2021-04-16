62de54f37918a812f6a55d0b051bb0427b21633f
eskil <mail.eskil@gmail.com>
2021-04-16 15:41:00 +0200

    add db logic

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,31 @@
 package model;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "users")
 public class User {
+
+    @Id
+    private Integer userID;
+
+    private String username;
+
+    private String password;
+
+    public User(Integer userID, String username, String password) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {}
+
+    @Override
+    public String toString() {
+        return "User: " + username + "password=" + password + "]";
+    }
 }
