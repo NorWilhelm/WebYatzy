2c86e138d7c4892514a5f49112baf60a97f22a9b
eskil <mail.eskil@gmail.com>
2021-04-16 15:51:09 +0200

    Add dao for user

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,29 @@
 package dao;
 
+import model.User;
+
+import java.util.List;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
 public class UserDao {
+
+    @PersistenceContext(name = "userPU")
+    private EntityManager em;
+
+    public List<User> fetchAllUsers() {
+        return em.createQuery("SELECT user FROM User user", User.class).getResultList();
+    }
+
+    public User fetchUser(Integer userID) {
+        return em.find(User.class, userID);
+    }
+
+    public void CreateUser(User nyBruker) {
+        em.persist(nyBruker);
+    }
 }
