af4322e10c0fdd3ea55dcc1d022ec4fe7fba7e2f
eskil <mail.eskil@gmail.com>
2021-04-21 17:50:21 +0200

    change url to prevent naming conflict

--- src/main/java/servlets/LobbyListServlet.java
+++ src/main/java/servlets/LobbyListServlet.java
@@ -26,7 +26,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "lobbyList", value = "/lobbies")
+@WebServlet(name = "lobbyList", value = "/lobbiesServlet")
 public class LobbyListServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
