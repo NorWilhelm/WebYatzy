742cb538b6524aca64636b266c14f9d95c56a939
joolr <joolrab@gmail.com>
2021-04-27 10:31:14 +0200

    Printout of calculateScore results in Test

--- src/main/java/utility/Test.java
+++ src/main/java/utility/Test.java
@@ -14,20 +14,20 @@ public class Test {
 			dice4.roll();
 			dice5.roll();
 			
-			Util.calculateScore(1, 1, 1, 1, 1, 1);
-			Util.calculateScore(2, 1, 4, 1, 1, 2);
-			Util.calculateScore(3, 1, 3, 2, 1, 3);
-			Util.calculateScore(4, 1, 1, 6, 6, 5);
-			Util.calculateScore(5, 1, 4, 3, 7, 6);
-			Util.calculateScore(6, 3, 2, 6, 5, 5);
-			Util.calculateScore(7, 1, 3, 1, 1, 2);
-			Util.calculateScore(8, 2, 2, 1, 1, 1);
-			Util.calculateScore(9, 3, 3, 3, 5, 6);
-			Util.calculateScore(10, 1, 6, 6, 2, 2);
-			Util.calculateScore(11, 5, 2, 2, 1, 1);
-			Util.calculateScore(12, 6, 4, 6, 4, 6);
-			Util.calculateScore(13, 1, 6, 5, 5, 1);
-			Util.calculateScore(14, 6, 4, 4, 4, 1);
-			Util.calculateScore(15, 1, 4, 2, 1, 3);
+			System.out.println(Util.calculateScore(1, 1, 1, 1, 1, 1));
+			System.out.println(Util.calculateScore(2, 1, 4, 1, 1, 2));
+			System.out.println(Util.calculateScore(3, 1, 3, 2, 1, 3));
+			System.out.println(Util.calculateScore(4, 1, 1, 6, 6, 5));
+			System.out.println(Util.calculateScore(5, 1, 4, 3, 7, 6));
+			System.out.println(Util.calculateScore(6, 3, 2, 6, 5, 5));
+			System.out.println(Util.calculateScore(7, 1, 3, 1, 1, 2));
+			System.out.println(Util.calculateScore(8, 2, 2, 1, 1, 1));
+			System.out.println(Util.calculateScore(9, 3, 3, 3, 5, 6));
+			System.out.println(Util.calculateScore(10, 1, 6, 6, 2, 2));
+			System.out.println(Util.calculateScore(11, 5, 2, 2, 1, 1));
+			System.out.println(Util.calculateScore(12, 6, 4, 6, 4, 6));
+			System.out.println(Util.calculateScore(13, 1, 6, 5, 5, 1));
+			System.out.println(Util.calculateScore(14, 6, 4, 4, 4, 1));
+			System.out.println(Util.calculateScore(15, 1, 4, 2, 1, 3));
 		}
 }
